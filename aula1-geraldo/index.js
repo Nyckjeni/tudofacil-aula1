e7e866c65350b5fef81e3b401769d4ea9842385d
Nyckjeni <nycole.jenifer366@gmail.com>
2025-02-17 20:15:06 -0300

    responsividade

--- aula1-geraldo/index.js
+++ aula1-geraldo/index.js
@@ -1,12 +1,4 @@
-// Seleciona o elemento com a classe "hamburguer" (o botão do menu)
-const hamburguer = document.querySelector(".hamburguer"); 
-
-// Seleciona o elemento com a classe "nav" (a barra de navegação)
-const nav = document.querySelector(".nav");
-
-// Adiciona um ouvinte de evento ao botão do hambúrguer
-hamburguer.addEventListener("click", () => {
-    // Alterna a classe "active" na barra de navegação (ativando ou desativando a exibição do menu)
-    nav.classList.toggle("active");
+document.getElementById('hamburguer').addEventListener('click', function() {
+    var menuItems = document.getElementById('menuItems');
+    menuItems.classList.toggle('active'); 
 });
-
